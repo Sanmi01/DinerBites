c435324a4a910461c4c92d5479f83645d72e1ac7
Sanmi01 <supersanmi01@gmail.com>
2022-04-27 07:09:29 +0100

    made adjustments

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -12,9 +12,9 @@ const About = () => {
                     <h4>Our Story</h4>
                     <h2>Few words about us</h2>
                     <hr />
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.
-                    Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.
-                    </p>
+                    <p>Since 1973, DinerBites has been the go-to diner for a lot of people. Our diner serves breakfast all day, in addition to wholesome and flavorful dining options for lunch and dinner. From burgers to salads, seafood to pastas, you’ll find all kinds of hearty meals prepared fresh at The Spot Restaurant. Our diner also has a full bakery with delicious baked goods and other treats, including our famous cheesecake. Sounds delicious, right?
+
+We’re here to serve you the best food around, whenever you’re looking for a great experience.</p>
                     <Link className="product-button" to="/products">Explore Our Menu</Link>
                 </div>
                 <div className="col-md-6 p-3">
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import "./Home.css"
+import {Link} from 'react-router-dom';
 
 
 const Home = () => {
     return (
-        <Container className="vh-100 py-3 px-5" id="home-page">
+        <Container className="py-3 px-5" id="home-page">
             <div className ="hero">
                 <div className='hero__content'>
                     <div className='hero__text'>
                         <h1>DinerBites</h1>
                         <h3>Exclusive kitchen</h3>
-                        <p>Our Restaurant meals provide larger 
-                            portion sizes and many more calories, Our Restaurant meals provide larger 
-                            portion sizes and many more calories
+                        <p>Craving some delicious food? Maybe you’re in the mood for a juicy steak? No matter what kind of meal you have in mind, <br />DinerBites is ready to prepare it for you.
                         </p>
-                        <button> Order Now</button>
+                        <Link to="/products"><button> Order Now</button></Link>
                         
 
                     </div>
